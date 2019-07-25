9df95ba3ad53d9dc705e3babdb57e6d7b326c975
Zeeshan Aslam <zeeshan.sulehria@gmail.com>
2019-07-25 17:27:19 +0500

    committing basic animation

---
+++ App.js
@@ -0,0 +1,90 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View, Animated, PanResponder, Dimensions} from 'react-native';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+
+  translateX = new Animated.Value(0);
+  _panResponder = PanResponder.create({
+    onMoveShouldSetResponderCapture: () => true,
+    onMoveShouldSetPanResponderCapture: () => true,
+    onPanResponderMove: Animated.event([null, {dy: this.translateX}]),
+    onPanResponderRelease: (e, {vx, dx}) => {
+      const screenWidth = Dimensions.get("window").width;
+      if (Math.abs(vx) >= 0.5 || Math.abs(dx) >= 0.5 * screenWidth) {
+        Animated.timing(this.translateX, {
+          toValue: dx > 0 ? screenWidth : -screenWidth,
+          duration: 200
+        }).start(this.props.onDismiss);
+      } else {
+        Animated.spring(this.translateX, {
+          toValue: 0,
+          bounciness: 10
+        }).start();
+      }
+    }
+  });
+
+  render() {
+    return (
+      <View style={styles.container}>
+        {/* <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>{instructions}</Text> */}
+        <Animated.View style={[styles.bottomMenu, {transform: [{translateY: this.translateX}]}]} {...this._panResponder.panHandlers}>
+          <View style={styles.nowButton}> 
+            <Text style={styles.nowButtonText}>NOW</Text> 
+          </View>
+        </Animated.View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  bottomMenu: {
+    position: "absolute",
+    bottom: 0,
+    alignSelf: "center",
+    backgroundColor: 'red',
+  },
+  nowButton: {
+    justifyContent: "center",
+    alignItems: 'center',
+    borderRadius: 100,
+    backgroundColor: 'blue',
+    height: 100,
+    width: 100,
+  },
+  nowButtonText: {
+
+  }
+});
